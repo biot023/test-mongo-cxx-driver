fa78292a347a214796bae389e76d64b090c8cbaa
douglivesey <biot023@gmail.com>
2013-04-05 16:27:29 +0100

    Getting it all working

--- src/tutorial.cpp
+++ src/tutorial.cpp
@@ -1,16 +1,39 @@
 #include <cstdlib>
 #include <iostream>
+#include <string>
+#include <vector>
 #include "mongo/client/dbclient.h"
 
-void run() {
-  mongo::DBClientConnection c;
-  c.connect( "localhost" );
+namespace runner {
+  using namespace std;
+  using namespace mongo;
+  
+  class Runner
+  { 
+  public:
+    void operator() () const
+    {
+      vector<string> names( { "Peter", "Michael", "Satan", "Horsey", "Genevieve" } );
+      DBClientConnection conn;
+      conn.connect( "localhost" );
+      for ( size_t i( 0 ) ; i < 100 ; ++ i )
+        conn.insert( "tutorial.people",
+                     BSON( GENOID
+                           << "name"       << names[i % 5]
+                           << "age"        << ( 31 + static_cast<int>( i % 5 ) )
+                           << "created_at" << DATENOW ) );
+      cout << "Count of people: " << conn.count( "tutorial.people" ) << endl;
+      auto cursor( conn.query( "tutorial.people", BSONObj() ) );
+      while ( cursor->more() )
+        cout << cursor->next().toString() << endl;
+    }
+  };  
 }
 
 int main() {
   try {
-    run();
-    std::cout << "connected ok" << std::endl;
+    runner::Runner()();
+    std::cout << "All good." << std::endl;
   } catch( const mongo::DBException &e ) {
     std::cout << "caught " << e.what() << std::endl;
   }
